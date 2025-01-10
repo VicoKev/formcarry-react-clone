894e0e5c72ca6cfeb31697d9b65f4c6f82b2e269
vicokev <kevinpatinvoh@gmail.com>
2025-01-10 23:47:06 +0100

    add assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// import logo from '../assets/logo.svg'
+// import { navLinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.jsx
@@ -0,0 +1,62 @@
+export const navLinks = [
+  { href: "#", text: "Examples" },
+  { href: "#", text: "Form Generator" },
+  { href: "#", text: "Integrations" },
+  { href: "#", text: "Docs" },
+  { href: "#", text: "Pricing" },
+];
+
+export const productLinks = [
+  { href: "#", text: "Frequently Asked Questions" },
+  { href: "#", text: "Pricing" },
+  { href: "#", text: "Dashboard" },
+  { href: "#", text: "Contact Us" },
+];
+
+export const formGeneratorLinks = [
+  { href: "#", text: "Contact Form Generator" },
+  { href: "#", text: "Survey Form Generator" },
+  { href: "#", text: "RSVP Form Generator" },
+  { href: "#", text: "All Form Generators" },
+];
+
+export const guideLinks = [
+  { href: "#", text: "How to add a contact form to your Shopify website" },
+  { href: "#", text: "How to build a simple HTML contact form" },
+  { href: "#", text: "How to create a file upload form" },
+  { href: "#", text: "How to upload files from your HTML form using Base64 Encoding" },
+  { href: "#", text: "How to create email templates" },
+];
+
+export const formCodeLinks = [
+  { href: "#", text: "HTML Contact Form" },
+  { href: "#", text: "React Contact Form" },
+  { href: "#", text: "NextJS Contact Form" },
+  { href: "#", text: "Vue Contact Form" },
+  { href: "#", text: "Jekyll Contact Form" },
+  { href: "#", text: "Gridsome Contact Form" },
+  { href: "#", text: "Gatsby Contact Form" },
+  { href: "#", text: "AlpineJS Contact Form" },
+  { href: "#", text: "All Form Examples" },
+];
+export const integrationLinks = [
+  { href: "#", text: "Google Sheets" },
+  { href: "#", text: "Mailchimp" },
+  { href: "#", text: "Airtable" },
+  { href: "#", text: "Discord" },
+  { href: "#", text: "Slack" },
+  { href: "#", text: "Notion" },
+  { href: "#", text: "Twilio" },
+  { href: "#", text: "All Apps & Integrations" },
+];
+export const resourceLinks = [
+  { href: "#", text: "Documentations" },
+  { href: "#", text: "Affiliate Program" },
+  { href: "#", text: "Press Kit" },
+  { href: "#", text: "Formcarry Blog" },
+  { href: "#", text: "System Status" },
+];
+
+
+
+
